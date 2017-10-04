6d820485db515c9a84a61d05ee4796a0f5f2753e
Logan <lwheeler1@carthage.edu>
2017-10-04 16:56:43 -0500

    Added a square function that squares the passed value

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void square(int val)
+{
+	//Add a void function called square() that takes an int value to be squared.Have the function print the squared value
+	int retval = val * val;
+	cout << "The squared value of " << val << " is: " << retval << endl;
+}
+
 int main()
 {
 }
