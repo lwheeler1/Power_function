2ba3ed8019daadaa174a8c92c1481263fd78e2e5
Logan <lwheeler1@carthage.edu>
2017-10-06 14:13:53 -0500

    Changed the power function to return an int rather that just cout the result. Tested in main and it works

--- Main.cpp
+++ Main.cpp
@@ -2,16 +2,23 @@
 
 using namespace std;
 
-void power(int base, int exponent)
+int power(int base, int exponent)
 {
 	//Change the name of the function to be called power and have it take an int base and an int exponent.Print the base number raised to the exponent power.
 	int retval = pow((double)base, exponent);
-	cout << base << "^" << exponent << " is: " << retval << endl;
+	return retval;
 }
 
 int main()
 {
-	power(2, 3);
+	if (power(2, 3) == 8)
+	{
+		cout << "Yeah boi!" << endl;
+	}
+	else
+	{
+		cout << "ERROR - you are an idiot!!" << endl;
+	}
 
 	system("pause");
 	return 0;
