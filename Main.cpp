4488772704dc1ed8eafdbb12b7c3166e0ef47bd5
Logan <lwheeler1@carthage.edu>
2017-10-06 14:15:14 -0500

    last step was wrong. I changed it back

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ int power(int base, int exponent)
 {
 	//Change the name of the function to be called power and have it take an int base and an int exponent.Print the base number raised to the exponent power.
 	int retval = pow((double)base, exponent);
-	return retval;
+	cout << retval << endl;
 }
 
 int main()
